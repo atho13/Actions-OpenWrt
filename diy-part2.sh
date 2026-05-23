bfecbb382622ded51ad3ceb6f6b482fad9d984e7
Atbel <kanaan.waropen@gmail.com>
2026-05-23 18:04:03 +0900

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+# Menggantikan 'make menuconfig' dengan otomatis mencentang paket QModem & Driver
+echo "CONFIG_PACKAGE_luci-app-qmodem=y" >> .config
+echo "CONFIG_PACKAGE_kmod-usb-net-rndis=y" >> .config
+echo "CONFIG_PACKAGE_kmod-usb-net-cdc-ether=y" >> .config
+echo "CONFIG_PACKAGE_kmod-usb-net-qmi-wwan=y" >> .config
+echo "CONFIG_PACKAGE_kmod-usb-net-cdc-mbim=y" >> .config
+echo "CONFIG_PACKAGE_usb-modeswitch=y" >> .config
